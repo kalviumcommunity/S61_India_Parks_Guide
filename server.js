020ab282331cc3f2919a0e86344fb3eb32f11b57
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-03-07 14:12:50 +0530

    error handling middleware

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express=require('express');
 const app=express();
 const port=3000;
 
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send('Something wrong...');
+});
+
 app.get('/ping',(req,res)=>{
     res.send("pong");
+});
+
+app.get('/test',(req,res)=>{
+    res.send("test response");
+});
+
+app.get('/error',(req,res,next)=>{
+    next(new Error('Testing error handling'));
 });
 
 app.listen(port,()=>{
