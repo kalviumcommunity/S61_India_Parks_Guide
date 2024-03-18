3399fd458555d3783bdacba07c8ad9c6e52c3bfc
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-03-18 09:50:44 +0530

    CRUD-API

---
+++ Schema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const nationalParkSchema =new Schema({
+    state: { type: String, required: true },
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    formed: { type: Number, required: true },
+    notableFeatures: { type: String, required: true },
+    fauna: { type: [String], required: true },
+    riversAndLakes: { type: [String], required: true }
+  }
+);
+  const parkModel=mongoose.model("Place",nationalParkSchema);
+  module.exports = { parkModel };
